144d5468279e952b9974cb1f1c55d955a19d448c
Ashish-dev-48 <prepnudge@gmail.com>
2024-11-16 20:44:41 +0530

    Update auth-middleware.js

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
     if (!token) {
         return res.status(401).json({ message: "Unauthorized, token not provided" });
     }
