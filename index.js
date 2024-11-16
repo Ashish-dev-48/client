cb1cc0c20befbc7017726472a05c2c22186002ab
Ashish-dev-48 <prepnudge@gmail.com>
2024-11-16 19:57:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import express from "express"
+import mongoConnect from './db.js';
+import errorHandler from './middleware/error-middleware.js';
+import userRouter from './router/user.js';
+import messageRouter from './router/message.js';
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+import { app, server } from "./socket/index.js"
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URI,
+    credentials: true,
+};
+
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api", userRouter)
+app.use("/api", messageRouter)
+app.use(errorHandler)
+
+server.listen(process.env.PORT, async () => {
+    await mongoConnect(process.env.MONGOURI)
+    console.log(`Backend is Live 🎉🎉`)
+})
