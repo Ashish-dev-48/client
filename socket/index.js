d70383899e2858e4c5a5401fccfa7946764ac34b
Ashish-dev-48 <prepnudge@gmail.com>
2024-11-17 02:14:48 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -8,7 +8,7 @@ export const server = http.createServer(app)
 const userSocketMap = {};
 const io = new Server(server, {
     cors: {
-        origin: *,
+        origin: '*',
         credentials: true
     }
 })
